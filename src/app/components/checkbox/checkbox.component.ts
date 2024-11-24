0b319de4372bebd9071cc1b0d9c7c1a81f2a6196
e.pichuzhkina <e.pichuzhkina@timebook.ru>
2024-11-24 17:08:44 +0300

    added checkbox

--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -4,24 +4,41 @@ import {
     Input,
     OnInit,
     Output,
-    EventEmitter
+    EventEmitter,
+	Self,
+	Optional,
+	ElementRef,
+	Renderer2,
 } from '@angular/core';
+import {
+    DefaultValueAccessor,
+    FormControl,
+    NgControl
+} from '@angular/forms';
 
 
 @Component({
-  selector: 'app-checkbox',
-  imports: [],
-  templateUrl: './checkbox.component.html',
-  styleUrl: './checkbox.component.scss'
+	standalone: true,
+	selector: 'app-checkbox',
+	imports: [],
+	templateUrl: './checkbox.component.html',
+	styleUrl: './checkbox.component.scss'
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent extends DefaultValueAccessor implements OnInit {
 	@Output() indeterminateChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 	@Input() label: string = '';
 	@Input() caption: string = '';
 	@Input() readOnly: boolean = false;
 
-	public value!: string;
-
+	private _value: boolean = false;
+    @Input() set value(value: boolean) {
+        this._value = value;
+    }
+    public get value(): boolean {
+		console.log(this.control);
+		console.log(this.control.value);
+        return !!this.control.value;
+    }
 
 	private _indeterminate: boolean = false;
 		@Input() get indeterminate(): boolean {
@@ -33,14 +50,20 @@ export class CheckboxComponent implements OnInit {
 		this.indeterminateChange.emit(this._indeterminate);
 	}
 
-
 	constructor(
+		private readonly renderer: Renderer2,
+        private readonly elementRef: ElementRef<HTMLElement>,
 		private readonly cd: ChangeDetectorRef,
+		@Self()
+        @Optional()
+        public readonly control: NgControl,
 	) {
-		
+		super(renderer, elementRef, true);
+		this.control.valueAccessor = this;
 	}
 
 	public ngOnInit(): void {
+		console.log(this.control.value);
 		
 	}
 
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,16 +1,30 @@
-import {
-	Component,
-	ChangeDetectionStrategy,
-  } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
   import { CheckboxComponent } from '../components/checkbox/checkbox.component';
+  import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-tabs',
-	imports: [CheckboxComponent],
+	imports: [
+		CheckboxComponent,
+		FormsModule,
+        ReactiveFormsModule,
+	],
 	templateUrl: './tabs.component.html',
 	styleUrl: './tabs.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabsComponent {
+	public label: string = 'Label';
+	public caption: string = 'Caption';
 
+	constructor(
+        
+    ) {
+
+    }
+
+	protected form = new FormGroup({
+        checkbox: new FormControl(false),
+
+    });
 }
